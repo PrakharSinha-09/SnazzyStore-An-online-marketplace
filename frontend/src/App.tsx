75760c678c81c4089e917a8a29072ce63c89a544
Prakhar Sinha <prakhar.sinha2k2@gmail.com>
2024-02-21 17:13:48 +0530

    Header component done with little styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import { lazy,Suspense } from "react"
 
 import Loader from "./components/Loader"
+import Header from "./components/Header"
 
 const Home = lazy(()=> import ("./pages/Home"))
 const Search = lazy(()=> import ("./pages/Search"))
@@ -29,7 +30,7 @@ const TransactionManagement = lazy(
 const App = () => {
   return (
     <Router>
-      {/* Header */}
+      <Header />
 
       <Suspense fallback={<Loader />}>
         <Routes>
