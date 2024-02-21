da5827142b344c1ec5a10b2cab178e4f46a4c9e4
Prakhar Sinha <prakhar.sinha2k2@gmail.com>
2024-02-21 15:34:38 +0530

    introduced some routings

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+
+import Home from "./pages/Home"
+import Search from "./pages/Search"
+import Cart from "./pages/Cart"
+
 const App = () => {
   return (
-    <div>App</div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/search" element={<Search />}></Route>
+        <Route path="/cart" element={<Cart />}></Route>
+      </Routes>
+    </Router>
   )
 }
 
