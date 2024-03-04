703bd490b1b8369c472109e1d33866a99c21b553
Prakhar Sinha <prakhar.sinha2k2@gmail.com>
2024-03-04 17:00:05 +0530

    Cart UI Component

--- frontend/src/pages/Cart.tsx
+++ frontend/src/pages/Cart.tsx
@@ -1,8 +1,86 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { VscError } from "react-icons/vsc";
+import CartItems from "../components/CartItems";
+import { Link } from "react-router-dom";
+const cartItems = [
+  {
+    productId:"dsdedsd",
+    photo:"https://m.media-amazon.com/images/I/71vFKBpKakL._SX522_.jpg",
+    name:"MacBook",
+    price:3000,
+    quantity:4,
+    stock:10,
+
+  }
+];
+const subtotal = 4000;
+const tax = Math.round(subtotal * 0.18);
+const shippingCharges=200;
+const discount=400
+const total = subtotal + tax + shippingCharges;
 
 const Cart = () => {
+
+  const[couponCode,setCouponCode]=useState<string>("")
+  const[isValidCouponCode,setIsValidCouponCode]=useState<boolean>(false)
+
+  useEffect(()=>{
+
+    const timeOutID=setTimeout(()=>{
+      if(Math.random()>0.5) setIsValidCouponCode(true)
+      else setIsValidCouponCode(false)
+    })
+
+      return () => {
+        clearTimeout(timeOutID);
+        setIsValidCouponCode (false);
+      };
+  },[couponCode]);
+
   return (
-    <div>Cart</div>
+    <div className="cart">
+      <main>
+        {
+          cartItems.length>0 ? (
+            cartItems.map((i,idx)=> <CartItems key={idx} cartItem={i} /> )
+          ):
+          (
+            <h1>Cart is Empty!</h1>
+          )
+        }
+      </main>
+      <aside>
+        <p>Subtotal: ₹{subtotal}</p>
+        <p>Shipping Charges: ₹{shippingCharges}</p>
+        <p>Tax:  ₹{tax}</p>
+        <p>
+          Discount: <em>- ₹{discount}</em>
+        </p>
+        <p>
+          <b>Total:  ₹{total}</b>
+        </p>
+
+        <input 
+          type="text" 
+          placeholder="Coupon Code"
+          value={couponCode} 
+          onChange={(e)=>setCouponCode(e.target.value)}
+        />
+
+        {couponCode &&
+          (isValidCouponCode ? (
+            <span className="green">
+              ₹{discount} off using the <code>{couponCode}</code>
+            </span>
+          ) : (
+            <span className="red">
+              Invalid Coupon <VscError />
+            </span>
+        ))}
+
+      {cartItems.length>0 && <Link to={'/shipping'}>Checkout</Link>}
+      </aside>
+    </div>
   )
 }
 
