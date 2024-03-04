4d94daec8f6956d67e732187c20bcc1fb2a3898d
Prakhar Sinha <prakhar.sinha2k2@gmail.com>
2024-03-04 20:57:29 +0530

    Shipping Component Added!

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,6 +7,7 @@ import Header from "./components/Header"
 const Home = lazy(()=> import ("./pages/Home"))
 const Search = lazy(()=> import ("./pages/Search"))
 const Cart = lazy(()=> import ("./pages/Cart"))
+const Shipping=lazy(()=> import ("./pages/Shipping"))
 
 //Admin routes importing
 const Dashboard = lazy(() => import("./pages/admin/dashboard"));
@@ -38,6 +39,10 @@ const App = () => {
           <Route path="/search" element={<Search />}></Route>
           <Route path="/cart" element={<Cart />}></Route>
 
+          {/* Logged In User Routes */}
+          <Route>
+            <Route path="/shipping" element={<Shipping />}></Route>
+          </Route>
 
 
             <Route
