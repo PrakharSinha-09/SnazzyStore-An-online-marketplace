c38588f55f53bc7ece90d7df080f406ee6e73419
Prakhar Sinha <prakhar.sinha2k2@gmail.com>
2024-03-04 15:43:40 +0530

    Home Component & ProductCard Component along with the styling!

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,8 +1,34 @@
-
+import { Link } from "react-router-dom"
+import ProductCard from "../components/ProductCard"
 const Home = () => {
+
+  const addToCartHandler=()=>{
+    
+  }
   return (
-    <div>home</div>
-  )
+
+    <div className="home">
+      <section></section>
+      <h1>
+      Latest Products
+        <Link to="/search" className="findmore">
+        More
+        </Link>
+      </h1>
+
+      <main>
+      <ProductCard
+        productId="adasdasd"
+        name="Macbook"
+        price={4545}
+        stock={435}
+        handler={addToCartHandler}
+        photo="https://m.media-amazon.com/images/I/71vFKBpKakL._SX522_.jpg" 
+      />
+
+      </main>
+  </div>
+    )
 }
 
 export default Home
